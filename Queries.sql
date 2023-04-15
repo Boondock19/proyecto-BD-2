481cfa6a9137aba2c2df78b148b01a5dd55441ad
Boondock19 <josematiasgonzalez20@gmail.com>
2023-04-14 21:31:56 -0400

    Se puede verificar resultado de un select en el procedure

--- Queries.sql
+++ Queries.sql
@@ -4,38 +4,38 @@
 -- Selects para verificar la insercion de los datos.
 
 --  First_names
-SELECT * FROM First_name;
+-- SELECT * FROM First_name;
 
---  Last_names
-SELECT * FROM Last_name;
+-- --  Last_names
+-- SELECT * FROM Last_name;
 
--- Emails
-SELECT * FROM Email;
+-- -- Emails
+-- SELECT * FROM Email;
 
--- Passwords
-SELECT * FROM Passwords;
+-- -- Passwords
+-- SELECT * FROM Passwords;
 
--- Usernames
-SELECT * FROM Username;
+-- -- Usernames
+-- SELECT * FROM Username;
 
--- Phone_numbers
-SELECT * FROM Phone_number;
+-- -- Phone_numbers
+-- SELECT * FROM Phone_number;
 
--- Food
-SELECT * FROM Food;
+-- -- Food
+-- SELECT * FROM Food;
 
--- Brand_names
-SELECT * FROM Brand_name;
+-- -- Brand_names
+-- SELECT * FROM Brand_name;
 
--- confirmation_code
-SELECT * FROM confirmation_code;
+-- -- confirmation_code
+-- SELECT * FROM confirmation_code;
 
--- Employee_code
-SELECT * FROM Employee_code;
+-- -- Employee_code
+-- SELECT * FROM Employee_code;
 
--- Unit
+-- -- Unit
 
-SELECT * FROM Unit;
+-- SELECT * FROM Unit;
 
 
 CALL insert_customers(5);
--- storedProcedures.sql
+++ storedProcedures.sql
@@ -16,11 +16,29 @@ $$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE PROCEDURE insert_customers(number_of_customers INT)
 AS $$
+DECLARE
+    items RECORD;
 BEGIN
 
-    SELECT first_name, last_name FROM First_name,Last_name
+    CREATE TEMP TABLE RESULT (
+        first_name VARCHAR(64),
+        last_name VARCHAR(64)
+    );
+
+
+    INSERT INTO RESULT (first_name, last_name) SELECT first_name,last_name FROM First_name,Last_name
     ORDER BY random() LIMIT number_of_customers;
 
+    FOR items IN SELECT * FROM RESULT LOOP
+    RAISE NOTICE 'first_name: %, last_name: %', quote_ident(items.first_name), quote_ident(items.last_name);
+    END LOOP;
+
+    -- FOR i IN SELECT * FROM RESULT LOOP
+    --     INSERT INTO Customer (first_name, last_name, user_name, password, time_inserted, confirmation_code, time_confirmed, contact_email, contact_phone, city_id, address, delivery_city_id, delivery_address)
+    --     VALUES (i.first_name, i.last_name, i.first_name || i.last_name, i.first_name || i.last_name, now(), i.first_name || i.last_name, now(), i.first_name || i.last_name || '@gmail.com', '123456789', 1, 'Calle 1', 1, 'Calle 2');
+    -- END LOOP;
+
+   
     
 END
 $$ LANGUAGE plpgsql;
